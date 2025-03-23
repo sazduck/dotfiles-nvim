46ac91ccb5fda76dcde707ff40cdbf4ab522525e
Sazduck <sergalets@gmail.com>
2025-03-23 14:30:29 +0400

    update keymaps; add new lsp;

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,28 @@ vim.g.maplocalleader = " "
 local set = vim.keymap.set
 
 set({'n', 'v'}, '<leader>y', '"+y')
+set({'n', 'v'}, '<leader>p', '"+0')
+
+-- Уменьшение ширины текущей панели (Alt + J)
+vim.keymap.set('n', '<M-j>', '<Cmd>resize -5<CR>', { desc = 'Resize window width down' })
+
+-- Увеличение ширины текущей панели (Alt + K)
+vim.keymap.set('n', '<M-k>', '<Cmd>resize +5<CR>', { desc = 'Resize window width up' })
+
+-- Уменьшение ширины текущей панели (Alt + H)
+vim.keymap.set('n', '<M-h>', '<Cmd>vertical resize -10<CR>', { desc = 'Resize window width left' })
+
+-- Увеличение ширины текущей панели (Alt + L)
+vim.keymap.set('n', '<M-l>', '<Cmd>vertical resize +10<CR>', { desc = 'Resize window width right' })
+
+-- Уменьшение ширины текущей панели (Alt + J)
+vim.keymap.set('n', '<M-C-j>', '<Cmd>resize -1<CR>', { desc = 'Resize window width down' })
+
+-- Увеличение ширины текущей панели (Alt + K)
+vim.keymap.set('n', '<M-C-k>', '<Cmd>resize +1<CR>', { desc = 'Resize window width up' })
+
+-- Уменьшение ширины текущей панели (Alt + H)
+vim.keymap.set('n', '<M-C-h>', '<Cmd>vertical resize -1<CR>', { desc = 'Resize window width left' })
+
+-- Увеличение ширины текущей панели (Alt + L)
+vim.keymap.set('n', '<M-C-l>', '<Cmd>vertical resize +1<CR>', { desc = 'Resize window width right' })
