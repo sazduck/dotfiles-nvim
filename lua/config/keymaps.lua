1951a4022ec87c8a2bb0f331a0d9e07233ac26e8
Sazduck <sergalets@gmail.com>
2025-04-10 21:39:09 +0400

    feat: add which-key, add some descs for keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,8 @@ vim.g.maplocalleader = " "
 
 local set = vim.keymap.set
 
-set({'n', 'v'}, '<leader>y', '"+y')
-set({'n', 'v'}, '<leader>p', '"+0')
+set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Yank to sys clipboard' })
+set({'n', 'v'}, '<leader>p', '"0p', { desc = 'Paste last yank' })
 
 -- Уменьшение ширины текущей панели (Alt + J)
 set('n', '<M-j>', '<Cmd>resize -5<CR>', { desc = 'Resize window width down' })
@@ -19,13 +19,13 @@ set('n', '<M-h>', '<Cmd>vertical resize -10<CR>', { desc = 'Resize window width 
 set('n', '<M-l>', '<Cmd>vertical resize +10<CR>', { desc = 'Resize window width right' })
 
 -- Уменьшение ширины текущей панели (Alt + J)
-set('n', '<M-C-j>', '<Cmd>resize -1<CR>', { desc = 'Resize window width down' })
+set('n', '<M-C-j>', '<Cmd>resize -1<CR>', { desc = 'Resize window width down by 1 symbol' })
 
 -- Увеличение ширины текущей панели (Alt + K)
-set('n', '<M-C-k>', '<Cmd>resize +1<CR>', { desc = 'Resize window width up' })
+set('n', '<M-C-k>', '<Cmd>resize +1<CR>', { desc = 'Resize window width up 1 symbol' })
 
 -- Уменьшение ширины текущей панели (Alt + H)
-set('n', '<M-C-h>', '<Cmd>vertical resize -1<CR>', { desc = 'Resize window width left' })
+set('n', '<M-C-h>', '<Cmd>vertical resize -1<CR>', { desc = 'Resize window width left 1 symbol' })
 
--- Увеличение ширины текущей панели (Alt + L)
-set('n', '<M-C-l>', '<Cmd>vertical resize +1<CR>', { desc = 'Resize window width right' })
+-- Увеличение ширины текущей панели (Alt + Ctrl + L)
+set('n', '<M-C-l>', '<Cmd>vertical resize +1<CR>', { desc = 'Resize window width right 1 symbol' })
--- lua/config/plugin-keymaps.lua
+++ lua/config/plugin-keymaps.lua
@@ -13,18 +13,18 @@ if pcall(require, "telescope") then
 end
 
 if pcall(require, "neo-tree") then
-  set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", { silent = true })
+  set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", { silent = true, desc = "Open explorer"  })
 end
 
 if pcall(require, "lspconfig") then
-  set("n", "gh", vim.lsp.buf.hover, {})
-  set("n", "gd", vim.lsp.buf.definition, {})
-  set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-  set("n", "<leader>r", vim.lsp.buf.rename, {})
+  set("n", "gh", vim.lsp.buf.hover, { desc = 'Hover' })
+  set("n", "gd", vim.lsp.buf.definition, { desc = 'Go to definition' })
+  set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = 'Code actions' })
+  set("n", "<leader>r", vim.lsp.buf.rename, { desc = 'Rename variable' })
 end
 
 if pcall(require, "null-ls") then
-  set({"n", "v"}, "<leader>cf", vim.lsp.buf.format, {})
+  set({"n", "v"}, "<leader>cf", vim.lsp.buf.format, { desc = 'Code fromat' })
 end
 
 if pcall(require, "gitsigns") then
@@ -38,7 +38,7 @@ if pcall(require, "gitsigns") then
     end)
 
     return "<Ignore>"
-  end, {expr=true})
+  end, { expr=true, desc = 'next hunk' })
 
   set("n", "[c", function ()
     if vim.wo.diff then return "[c" end
@@ -48,27 +48,27 @@ if pcall(require, "gitsigns") then
     end)
 
     return "<Ignore>"
-  end, {expr=true})
+  end, { expr=true, desc = 'prev hunk' })
 
-  set('n', '<leader>hs', gs.stage_hunk)
-  set('n', '<leader>hr', gs.reset_hunk)
+  set('n', '<leader>hs', gs.stage_hunk, { desc = 'stage hunk' })
+  set('n', '<leader>hr', gs.reset_hunk, { desc = 'reset hunk' })
   set("v", "<leader>hs", function()
     gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-  end)
+  end, { desc = 'stage hunk' })
 	set("v", "<leader>hr", function()
 		gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-	end)
+	end, { desc = 'reset hunk' })
 
-  set("n", "<leader>ha", gs.stage_hunk)
-  set("n", "<leader>hu", gs.undo_stage_hunk)
+  set("n", "<leader>ha", gs.stage_hunk, { desc = 'stage hunk' })
+  set("n", "<leader>hu", gs.undo_stage_hunk, { desc = 'undo stage hunk' })
 
-  set("n", "<leader>hS", gs.stage_buffer)
-  set("n", "<leader>hr", gs.reset_buffer)
+  set("n", "<leader>hS", gs.stage_buffer, { desc = 'stage buffer' })
+  set("n", "<leader>hr", gs.reset_buffer, { desc = 'reset buffer' })
 
-  set("n", "<leader>hp", gs.preview_hunk)
+  set("n", "<leader>hp", gs.preview_hunk, { desc = 'preview hunk' })
 
-  set('n', '<leader>hb', function() gs.blame_line{full=true} end)
-  set('n', '<leader>hd', gs.diffthis)
+  set('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = 'Git blame line' })
+  set('n', '<leader>hd', gs.diffthis, { desc = 'diff this' })
 
-  set({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  set({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'selet hunk'})
 end
