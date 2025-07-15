6a69a71660bd6780d2199819857a4da86dc006f7
sazduck <sergalets@gmail.com>
2025-07-15 21:11:47 +0400

    feat(treestitter): add some f-ing ensure_installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,21 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "javascript",
+        "json",
+        "jsonc",
+        "lua",
+        "markdown",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+      },
       auto_install = true,
       sync_install = false,
       highlight = { enable = true },
-      indent = { enable = true }
+      indent = { enable = true },
     })
   end,
 }
