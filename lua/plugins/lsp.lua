f6e32debe6f6a88af702dfd649bee9a23e5c5ba5
Sazduck <sergalets@gmail.com>
2025-08-27 18:13:51 +0400

    Revert "format: fix lsp.lua"
    This reverts commit 5e9ca4344ebe953e9990cf552c76c91c6284213c.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,65 +33,67 @@ return {
       ensure_installed = lsps,
     },
   },
-
+  
   {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      local borderStyle = "rounded"
-      vim.diagnostic.config({
-        virtual_text = true,
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-      })
-      local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-      ---@diagnostic disable-next-line: duplicate-set-field
-      function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-        opts = opts or {}
-        opts.border = borderStyle
-        return orig_util_open_floating_preview(contents, syntax, opts, ...)
-      end
+  "neovim/nvim-lspconfig",
+  lazy = false,
+  config = function()
+    local lspconfig = require("lspconfig")
+    local borderStyle = "rounded"
+    vim.diagnostic.config({
+      virtual_text = true,
+      signs = true,
+      underline = true,
+      update_in_insert = false,
+    })
+    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+    ---@diagnostic disable-next-line: duplicate-set-field
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+      opts = opts or {}
+      opts.border = borderStyle
+      return orig_util_open_floating_preview(contents, syntax, opts, ...)
+    end
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    require("mason-lspconfig").setup_handlers({
+      function(server_name) -- Обработчик по умолчанию
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+        })
+      end,
+      -- ⚠️ Для особых случаев (hyprlang, bash) оставляем кастомные настройки
+      ["hyprls"] = function()
+        lspconfig["hyprls"].setup({
+          capabilities = capabilities,
+          on_attach = function(_, bufnr)
+            vim.api.nvim_buf_set_option(bufnr, "filetype", "hyprlang")
+          end,
+        })
+      end,
+      ["bashls"] = function()
+        lspconfig["bashls"].setup({
+          capabilities = capabilities,
+          on_attach = function(_, bufnr)
+            vim.api.nvim_buf_set_option(bufnr, "filetype", "bash")
+          end,
+        })
+      end,
+    })
 
-      require("mason-lspconfig").setup_handlers({
-        function(server_name) -- Обработчик по умолчанию
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-        ["hyprls"] = function()
-          lspconfig["hyprls"].setup({
-            capabilities = capabilities,
-            on_attach = function(_, bufnr)
-              vim.api.nvim_buf_set_option(bufnr, "filetype", "hyprlang")
-            end,
-          })
-        end,
-        ["bashls"] = function()
-          lspconfig["bashls"].setup({
-            capabilities = capabilities,
-            on_attach = function(_, bufnr)
-              vim.api.nvim_buf_set_option(bufnr, "filetype", "bash")
-            end,
-          })
-        end,
-      })
-
-      vim.filetype.add({
-        pattern = {
-          [".*/hypr/.*%.conf"] = "hyprlang",
-          [".*/bash/.*"] = "bash",
-        },
-      })
-    end,
-    keys = {
-      { "gh",         vim.lsp.buf.hover,       desc = "Hover" },
-      { "gd",         vim.lsp.buf.definition,  desc = "Go to definition" },
-      { "<leader>ca", vim.lsp.buf.code_action, desc = "Code actions",    mode = { "n", "v" } },
-      { "<leader>r",  vim.lsp.buf.rename,      desc = "Rename variable" },
-    },
+    -- Удаляем автокоманды для hyprlang/bash — они теперь избыточны!
+    -- (Настройки выше обрабатывают filetype через on_attach)
+    vim.filetype.add({
+      pattern = {
+        [".*/hypr/.*%.conf"] = "hyprlang",
+        [".*/bash/.*"] = "bash",
+      },
+    })
+  end,
+  keys = {
+    { "gh",         vim.lsp.buf.hover,       desc = "Hover" },
+    { "gd",         vim.lsp.buf.definition,  desc = "Go to definition" },
+    { "<leader>ca", vim.lsp.buf.code_action, desc = "Code actions",    mode = { "n", "v" } },
+    { "<leader>r",  vim.lsp.buf.rename,      desc = "Rename variable" },
   },
+},
 }
